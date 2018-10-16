0fcd1196760e9e9fefa6f1e9892856f140034d1b
autonomous <autonomous666@gmail.com>
2018-10-16 14:04:46 -0700

    implemented jsonrpc 2.0

--- src/BionetProxyClient.js
+++ src/BionetProxyClient.js
@@ -6,26 +6,30 @@ const WebSocketClient = require('websocket').client;
 
 module.exports = class BionetClient {
     constructor() {
-        this.cbRegistry={}
-        this.connection=null
-        this.rpcStream=this.rpc
+        this.cbRegistry = {}
+        this.connection = null
+        this.rpcStream = this.rpc
     }
-    rpc(rpcRequest,cb) {
+    rpc(rpcRequest, cb) {
         if (this.connection && this.connection.connected) {
-            var msgId = Math.random().toString()
-            rpcRequest.msgId = msgId
-            this.cbRegistry[msgId]=cb
+            function getRndInteger(max, min) {
+                return Math.floor(Math.random() * (max - min + 1)) + min;
+            }
+            var msgId = getRndInteger(1, 9999999)
+            rpcRequest.jsonrpc = "2.0"
+            rpcRequest.id = msgId
+            this.cbRegistry[msgId.toString()] = cb
             this.connection.sendUTF(JSON.stringify(rpcRequest));
         } else {
             cb(new Error("not connected to proxy host"))
         }
     }
-    connect(proto,host,port,cb) {
+    connect(proto, host, port, cb) {
         var client = new WebSocketClient();
-        const self=this
+        const self = this
         client.on('connect', function (connection) {
             console.log('bionet proxy webSocket connected');
-            self.connection=connection
+            self.connection = connection
             connection.on('error', function (error) {
                 console.log("Connection Error: " + error.toString());
                 process.exit()
@@ -36,21 +40,32 @@ module.exports = class BionetClient {
             });
             function onMessage(message) {
                 if (message.type === 'utf8') {
-                    var jsonMsg = JSON.parse(message.utf8Data)
-                    var msgId = jsonMsg.msgId
-                    var cbResult = self.cbRegistry[msgId]
+                    var jsonRpcResponse = null
+                    try {
+                        jsonRpcResponse = JSON.parse(message.utf8Data)
+                    } catch (e) {
+                        var errorMsg = {
+                            code: -32700,
+                            message: "Invalid JSON was received by the client.",
+                            data: null
+                        }
+                        console.log('Error:', errorMsg)
+                        return
+                    }
+                    var msgId = jsonRpcResponse.id
+                    var cbResult = self.cbRegistry[msgId.toString()]
                     if (msgId && cbResult) {
-                        delete self.cbRegistry[msgId]
-                        cbResult(jsonMsg.err,jsonMsg.data)
+                        var keepCb = cbResult(jsonRpcResponse.error, jsonRpcResponse.params)
+                        if (!keepCb) delete self.cbRegistry[msgId.toString()]
                     } else {
-                        console.log('no msgId:',jsonMsg)
+                        console.log('no msgId:', jsonRpcResponse)
                     }
                 } else {
                     console.log("invalid message type received")
                 }
             }
             connection.on('message', onMessage)
-            if (cb) cb(null,connection)
+            if (cb) cb(null, connection)
             return connection
         });
         var wsUrl = proto + host + ":" + port + "/"
